cfbc8f529bd8aeb1311fc928f7b2a744fb0f8eaa
eddieyg <eddiezhao@foxmail.com>
2020-09-13 16:30:53 +0800

    readme fix

--- example/index.js
+++ example/index.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 const PDFTypeset = require('../src/main')
 
-// PDFTypeset.toTimes({
-//   sourcePath: 'source.pdf', 
-//   insertIndex: -2,
-//   times: 5,
-//   fillPagePath: 'report.pdf',
-// }, (data, errMsg) => {
-//   if (data) fs.writeFileSync(`source-after.pdf`, data)
-// })
+PDFTypeset.toTimes({
+  sourcePath: 'source.pdf', 
+  insertIndex: -2,
+  times: 5,
+  fillPagePath: 'report.pdf',
+}, (data, errMsg) => {
+  if (data) fs.writeFileSync(`source-after.pdf`, data)
+})
 
 PDFTypeset.toBook({
   sourcePath: 'report.pdf', 
